ae0eb3ef580ca807406c6944b7c6e414214f862a
Mariano Simone <undefined.behaviour@gmail.com>
2010-01-13 11:59:50 -0300

    -

---
+++ app/controllers/users_controller.php
@@ -0,0 +1,25 @@
+<?php
+
+class UsersController extends AppController
+{
+    var $name = 'Users';
+    var $uses = array();
+    
+    function cms_index()
+    {
+    }
+    
+    function cms_add()
+    {
+    }
+    
+    function cms_edit($id = null)
+    {
+    }
+    
+    function cms_remove($id = null)
+    {
+    }
+}
+
+?>
